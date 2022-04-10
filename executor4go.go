ee081043b212613a1d878a78b8194f7b4f7c6b5b
LiuYong <793875613@qq.com>
2022-04-10 12:34:19 +0800

    add README.md and README_ZH.md

---
+++ executor4go.go
@@ -0,0 +1,13 @@
+package boot4go_xxljob_executor
+
+/**
+* golang-sample源代码，版权归锦翰科技（深圳）有限公司所有。
+* <p>
+* 文件名称 : executor4go.go
+* 文件路径 :
+* 作者 : DavidLiu
+× Email: david.liu@ginghan.com
+*
+* 创建日期 : 2022/4/10 11:24
+* 修改历史 : 1. [2022/4/10 11:24] 创建文件 by NST
+*/
